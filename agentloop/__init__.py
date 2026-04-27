a477fb2afee40d8fa15b49abce6f316f2af087ac
Dipesh Babu <59379458+dipeshbabu@users.noreply.github.com>
2026-04-27 19:15:49 -0400

    Export integration recording helpers

--- agentloop/__init__.py
+++ agentloop/__init__.py
@@ -1,10 +1,22 @@
 from agentloop.client import AgentLoopClient, AgentLoopClientError
-from agentloop.tracer import AgentTrace, trace_agent, trace_model_call, trace_retry, trace_tool_call
+from agentloop.tracer import (
+    AgentTrace,
+    current_trace,
+    record_model_call,
+    record_tool_call,
+    trace_agent,
+    trace_model_call,
+    trace_retry,
+    trace_tool_call,
+)
 
 __all__ = [
     "AgentLoopClient",
     "AgentLoopClientError",
     "AgentTrace",
+    "current_trace",
+    "record_model_call",
+    "record_tool_call",
     "trace_agent",
     "trace_model_call",
     "trace_tool_call",
