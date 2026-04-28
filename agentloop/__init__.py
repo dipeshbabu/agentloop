5483ba94e0e5d6caf09806c669f3190712b4e005
Dipesh Babu <59379458+dipeshbabu@users.noreply.github.com>
2026-04-27 20:38:06 -0400

    Export doctor and auto instrumentation helpers

--- agentloop/__init__.py
+++ agentloop/__init__.py
@@ -1,5 +1,7 @@
+from agentloop.autoinstrument import InstrumentationResult, auto_instrument
 from agentloop.client import AgentLoopClient, AgentLoopClientError
 from agentloop.decorators import trace_model, trace_tool, traceable
+from agentloop.doctor import run_doctor
 from agentloop.runtime import get_last_error, get_runtime_config, init, reset_runtime
 from agentloop.tracer import (
     AgentTrace,
@@ -17,6 +19,8 @@ __all__ = [
     "AgentLoopClient",
     "AgentLoopClientError",
     "AgentTrace",
+    "InstrumentationResult",
+    "auto_instrument",
     "build_value_report",
     "current_trace",
     "get_last_error",
@@ -25,6 +29,7 @@ __all__ = [
     "record_model_call",
     "record_tool_call",
     "reset_runtime",
+    "run_doctor",
     "trace_agent",
     "trace_model",
     "trace_model_call",
